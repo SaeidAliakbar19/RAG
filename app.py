438fcba26bef2f7df39e352349307e94a5cec77e
saeid1819 <saeid.aliakbar74@gmail.com>
2024-03-25 12:00:54 +0330

    Language Update

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_text_chunks(text):
     text_splitter = CharacterTextSplitter (
         separator="\n",
         chunk_size = 1000,
-        chunk_overlap = 200,
+        chunk_overlap = 100,
         length_function = len
     )
     chunks = text_splitter.split_text(text)
@@ -37,7 +37,7 @@ def get_vectorstore(text_chunks):
     #embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks , embedding=embeddings )
     return vectorstore
-
+    
 def get_conversation_chain(vectorstore):
     llm = ChatOpenAI(model_name="gpt-4-0125-preview")
     memory = ConversationBufferMemory(memory_key='chat_history',return_messages=True)
@@ -61,7 +61,7 @@ def handle_userinput(user_question):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="بنداز ببینم چی داری؟", page_icon=":books:")
+    st.set_page_config(page_title="RAGE: Rade + RAG", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -70,17 +70,17 @@ def main():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = None
 
-    st.header("فایل هاتو بده ببینم چی داری؟")
-    user_question = st.text_input("حالا حرف حسابت چیه؟")
+    st.header("I can help you chat about your documents.")
+    user_question = st.text_input("Before we begin, please ensure you've uploaded your files from the sidebar.")
     if user_question:
         handle_userinput(user_question)
 
 
     with st.sidebar:
-        st.subheader("دری وری ها")
-        pdf_docs = st.file_uploader("بکن توم و فشارم بده",accept_multiple_files=True)
-        if st.button("فشار"):
-            with st.spinner("دارم فشار میخورم"):
+        st.subheader("Upload Your Files")
+        pdf_docs = st.file_uploader("Upload Your Files and Press Process",accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Please wait: it can take few minutes"):
                 # get the pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
@@ -92,7 +92,6 @@ def main():
                 
                 # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
-
     
 if __name__ == '__main__':
     main()
